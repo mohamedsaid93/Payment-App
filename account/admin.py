b2ef963dc2ef12d3c8cd1569e30aa8335c49aef2
Mohamed Said <msm.793@hotmail.com>
2023-10-25 09:42:46 +0100

    first commit

---
+++ account/admin.py
@@ -0,0 +1,21 @@
+from django.contrib import admin
+from account.models import Account, KYC
+from userauths.models import User
+#from import_export.admin import ImportExportModelAdmin
+
+# Register your models here.
+
+# class AccountAdminModel(ImportExportModelAdmin): # we might as well use admin.mideladmin instead
+class AccountAdminModel(admin.ModelAdmin):
+    list_editable = ['account_status', 'account_balance']
+    list_display = ['user', 'account_number', 'account_status', 'account_balance']
+    list_filter = ['account_status']
+
+
+# class KYCAdmin(ImportExportModelAdmin): # we might as well use admin.mideladmin instead
+class KYCAdmin(admin.ModelAdmin):
+    search_fields = ['full_name']
+    list_display = ['user', 'full_name'] 
+
+admin.site.register(Account, AccountAdminModel)
+admin.site.register(KYC, KYCAdmin)
